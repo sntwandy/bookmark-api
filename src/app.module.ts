2f94d3088899d532d7396bfdc7010a90feae7721
sntwandy <sntwandy@gmail.com>
2022-10-16 10:45:14 -0400

    feat: ✨Prisma module to globally to use in others module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule],
+  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
